d116a077eb8b4b7cb8e239a0d66eec0af4bd6f53
Xueyan23 <chintusinghrana1234567890@gmail.com>
2021-03-02 18:59:43 +0530

    Index file of php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+echo "Hello World!!!";
+?>
+<!-- index.php wali file automatically load hogi agar aap kisi particular file ka path nhi dere
+matlab khali project ko access krre toh by default index.php load hogi
+jaise
+http://localhost/phpByCodeWithHarry/CodeWithHarryPhp/
+ya http://localhost/phpByCodeWithHarry/CodeWithHarryPhp/index.php
+dono mein index file load hogi
+ -->
