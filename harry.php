159cff2ad1807dd7ba66de1cabc5721ed08831d2
Xueyan23 <chintusinghrana1234567890@gmail.com>
2021-03-03 23:16:32 +0530

    php intro

---
+++ harry.php
@@ -0,0 +1,7 @@
+<?php
+echo "Hello". 1+431 ." ". "This is Harry \" Bhai";//ismein Hello print nhi hoga Aur addition bhi nhi hoga khali 431 aur uske aage ki string print hoga
+echo "<br>";
+echo 1+431 ." ". "This is Harry Bhai";
+echo "<br>";
+echo 1+431 . " This is Harry Bhai";
+?>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 echo "Hello World!!!";
+echo "<br>";
+echo 5+34;
 ?>
 <!-- index.php wali file automatically load hogi agar aap kisi particular file ka path nhi dere
 matlab khali project ko access krre toh by default index.php load hogi
