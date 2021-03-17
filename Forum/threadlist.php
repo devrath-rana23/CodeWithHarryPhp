fdcaedecdd3e1d38f50408da9d175cad09c1abd9
Xueyan23 <chintusinghrana1234567890@gmail.com>
2021-03-18 01:19:25 +0530

    Threads

--- Forum/threadlist.php
+++ Forum/threadlist.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -25,12 +27,12 @@
                 doloremque maxime totam ipsam ad perferendis praesentium, modi magni dolorum ratione eos ducimus
                 tempora!</p>
             <hr class="my-4">
-            <p>    No Spam / Advertising / Self-promote in the forums. ...
-    Do not post copyright-infringing material. ...
-    Do not post “offensive” posts, links or images. ...
-    Do not cross post questions. ...
-    Do not PM users asking for help. ...
-    Remain respectful of other members at all times.
+            <p> No Spam / Advertising / Self-promote in the forums. ...
+                Do not post copyright-infringing material. ...
+                Do not post “offensive” posts, links or images. ...
+                Do not cross post questions. ...
+                Do not PM users asking for help. ...
+                Remain respectful of other members at all times.
             </p>
             <p class="lead">
                 <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
@@ -51,20 +53,36 @@
         </div>
 
         <div class="media my-3">
-        <img class="mr-3" src="./img/userDefaultImage.png" width="40px" alt="Generic placeholder image">
-        <div class="media-body">
-            <h5 class="mt-0">Unable to install pyaudio, error in windows</h5>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio,
-            vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec
-            lacinia congue felis in faucibus.
+            <img class="mr-3" src="./img/userDefaultImage.png" width="40px" alt="Generic placeholder image">
+            <div class="media-body">
+                <h5 class="mt-0">Unable to install pyaudio, error in windows</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus
+                odio,
+                vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
+                Donec
+                lacinia congue felis in faucibus.
+            </div>
         </div>
-    </div>
 
     </div>
-
     <?php include "partials/_footer.php"; ?>
 
-    <!-- Optional JavaScript -->
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js"
+        integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"
+        integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous">
+    </script>
+
+
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
@@ -75,6 +93,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
+
 </body>
 
 </html>
